067338ec6efe4a9d323fac44c123f6951df9faa2
1107891366 <1107891366@qq.com>
2025-02-22 16:17:39 +0000

    init and build workspace

---
+++ src/55.jump-game.cpp
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=cpp
+ * @lcpr version=30204
+ *
+ * [55] 跳跃游戏
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+    Solution solution;
+    
+
+
+}
+
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,3,1,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,1,0,4]\n
+// @lcpr case=end
+
+ */
+
