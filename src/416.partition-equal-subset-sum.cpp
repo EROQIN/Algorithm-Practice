294be077b70299523f4a5ce2138d878507a53b45
1107891366 <1107891366@qq.com>
2025-02-26 12:34:15 +0000

    416.分割等和子集

--- src/416.partition-equal-subset-sum.cpp
+++ src/416.partition-equal-subset-sum.cpp
@@ -23,25 +23,38 @@ using namespace std;
 #include <unordered_set>
 #include <utility>
 #include <vector>
+#include <numeric>
 // @lcpr-template-end
 // @lc code=start
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        //dp[i]的表示：容量为i的背包，所背的最大价值为dp[i]
-        int sum = 0;
-        vector<int> dp(10001,0);
-        for(auto& i : nums){
-            sum += i;
-        }
-        if(sum % 2 ==1)return false;
+        // //dp[i]的表示：容量为i的背包，所背的最大价值为dp[i]
+        // int sum = 0;
+        // vector<int> dp(10001,0);
+        // for(auto& i : nums){
+        //     sum += i;
+        // }
+        // if(sum % 2 ==1)return false;
+        // int target = sum/2;
+        // for(int i = 0;i<nums.size();++i){
+        //     for(int j = target;j>= nums[i];--j){
+        //         dp[j] = max(dp[j],dp[j-nums[i]] + nums[i]);
+        //     }
+        // }
+        // if(dp[target] == target) return true;
+        // return false;
+        
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2 == 1) return false;
         int target = sum/2;
-        for(int i = 0;i<nums.size();++i){
+        vector<int> dp(10001,0);
+        for (int i = 0; i < nums.size(); ++i){
             for(int j = target;j>= nums[i];--j){
-                dp[j] = max(dp[j],dp[j-nums[i]] + nums[i]);
+                dp[j] = max(dp[j],dp[j-nums[i]]+nums[i]);
             }
         }
-        if(dp[target] == target) return true;
+        if(dp[target]==target) return true;
         return false;
     }
 };
