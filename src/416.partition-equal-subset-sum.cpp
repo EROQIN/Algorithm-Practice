0f16b692ac6d32766eed130ba80c3f53895b8316
1107891366 <1107891366@qq.com>
2025-02-26 13:28:30 +0000

    416.分割等和子集

--- src/416.partition-equal-subset-sum.cpp
+++ src/416.partition-equal-subset-sum.cpp
@@ -52,6 +52,7 @@ public:
         for (int i = 0; i < nums.size(); ++i){
             for(int j = target;j>= nums[i];--j){
                 dp[j] = max(dp[j],dp[j-nums[i]]+nums[i]);
+                if(dp[target]==target) return true;
             }
         }
         if(dp[target]==target) return true;
