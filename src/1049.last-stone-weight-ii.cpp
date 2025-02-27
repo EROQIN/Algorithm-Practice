2543c5523755783d833a9a886d5aea3653bbb553
1107891366 <1107891366@qq.com>
2025-02-27 05:46:13 +0000

    1049.最后一块石头的重量II

--- src/1049.last-stone-weight-ii.cpp
+++ src/1049.last-stone-weight-ii.cpp
@@ -29,6 +29,15 @@ using namespace std;
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
+        // int sum = accumulate(stones.begin(),stones.end(),0);
+        // vector<int> dp(15001,0);
+        // int target = sum/2;
+        // for(int i=0;i<stones.size();++i){
+        //     for(int j = target;j>=stones[i];--j){
+        //         dp[j] = max(dp[j],dp[j-stones[i]]+stones[i]);
+        //     }
+        // }    
+        // return sum-dp[target]-dp[target];
         int sum = accumulate(stones.begin(),stones.end(),0);
         vector<int> dp(15001,0);
         int target = sum/2;
@@ -36,9 +45,9 @@ public:
             for(int j = target;j>=stones[i];--j){
                 dp[j] = max(dp[j],dp[j-stones[i]]+stones[i]);
             }
-        }    
-        return sum-dp[target]-dp[target];
-
+        }
+        return (sum - dp[target]) - dp[target];
+        
 
     }
 };
